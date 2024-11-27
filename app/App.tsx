81be21eaa8ecd79e5e1d3af647d6c5c68e9a26df
augustine-awuori <codewithaugustine@gmail.com>
2024-11-27 09:35:58 +0300

    Code refactor

--- app/App.tsx
+++ app/App.tsx
@@ -4,10 +4,10 @@ import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppNavigator } from "./navigation";
+import { initUsers } from "./hooks/useUsers";
 import { navigationTheme } from "./navigation";
 import { User, Users } from "./contexts/UsersContext";
 import { UsersContext } from "./contexts";
-import usersService from "./services/users";
 
 export default function App() {
   const [allUsers, setAllUsers] = useState<User[]>([]);
@@ -15,37 +15,7 @@ export default function App() {
   const [usersLoading, setUsersLoading] = useState(false);
 
   useEffect(() => {
-    function getVerifiedFirst(users: User[]): User[] {
-      return users.sort((a, b) => (b.verified ? 1 : 0) - (a.verified ? 1 : 0));
-    }
-
-    function getValidUsers(users: User[]): User[] {
-      return users
-        .filter(({ username }) => username !== "awuori")
-        .filter((u) => !u.invalid);
-    }
-
-    async function retrieveAllUsersInfo() {
-      try {
-        setUsersLoading(true);
-        const res = await usersService.getAllUsers();
-        setUsersLoading(false);
-
-        if (res.ok) {
-          setAllUsers(getVerifiedFirst(getValidUsers(res.data as User[])));
-
-          let users: Users = {};
-          (res.data as User[]).forEach(({ _id, username }) => {
-            if (!users[username]) users[username] = _id;
-          });
-          setUsers(users);
-        }
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    }
-
-    retrieveAllUsersInfo();
+    initUsers({ onLoad: setUsersLoading, setAllUsers, setUsers });
   }, []);
 
   return (
--- app/hooks/useUsers.ts
+++ app/hooks/useUsers.ts
@@ -1,6 +1,46 @@
 import { useContext } from "react";
 
+import { User, Users } from "../contexts/UsersContext";
 import { UsersContext } from "../contexts";
+import service from "../services/users";
+
+function getVerifiedFirst(users: User[]): User[] {
+  return users.sort((a, b) => (b.verified ? 1 : 0) - (a.verified ? 1 : 0));
+}
+
+function getValidUsers(users: User[]): User[] {
+  return users
+    .filter(({ username }) => username !== "awuori")
+    .filter((u) => !u.invalid);
+}
+
+export async function initUsers({
+  onLoad,
+  setAllUsers,
+  setUsers,
+}: {
+  onLoad: (loading: boolean) => void;
+  setUsers: (users: Users) => void;
+  setAllUsers: (users: User[]) => void;
+}) {
+  try {
+    onLoad(true);
+    const res = await service.getAllUsers();
+    onLoad(false);
+
+    if (res.ok) {
+      setAllUsers(getVerifiedFirst(getValidUsers(res.data as User[])));
+
+      let users: Users = {};
+      (res.data as User[]).forEach(({ _id, username }) => {
+        if (!users[username]) users[username] = _id;
+      });
+      setUsers(users);
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
 
 const useUsers = () => {
   const context = useContext(UsersContext);
