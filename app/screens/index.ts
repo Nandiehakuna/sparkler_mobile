9ebe328ef0f856b739c3064c6146dbd3f2291bba
augustine-awuori <codewithaugustine@gmail.com>
2024-12-02 11:39:06 +0300

    Remove unnecessary splash screen

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -3,7 +3,6 @@ export { default as ProfileMediaScreen } from "./ProfileMediaScreen";
 export { default as ProfileScreen } from "./ProfileScreen";
 export { default as ProfileSparklesAndResparklesScreen } from "./ProfileSparklesAndResparklesScreen";
 export { default as ProfileSparklesScreen } from "./ProfileSparklesScreen";
-export { default as SplashScreen } from "./SplashScreen";
 export { default as ThreadScreen } from "./ThreadScreen";
 export { default as TimelineScreen } from "./TimelineScreen";
 export { default as UsersScreen } from "./UsersScreen";
