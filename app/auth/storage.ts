8706893267b8782f099bf97348f46b62f800f479
augustine-awuori <codewithaugustine@gmail.com>
2024-12-11 17:05:23 +0300

    Store user token securely

--- app/auth/storage.ts
+++ app/auth/storage.ts
@@ -1,4 +1,4 @@
-// import * as SecureStore from "expo-secure-store";
+import * as SecureStore from "expo-secure-store";
 import { jwtDecode } from "jwt-decode";
 
 import { User } from "../contexts/UsersContext";
@@ -7,7 +7,7 @@ const key = "authToken";
 
 const storeToken = async (authToken: string) => {
   try {
-    // await SecureStore.setItemAsync(key, authToken);
+    await SecureStore.setItemAsync(key, authToken);
   } catch (error) {
     console.log("Error storing the auth token", error);
   }
@@ -15,8 +15,7 @@ const storeToken = async (authToken: string) => {
 
 const getToken = async () => {
   try {
-    return undefined;
-    // return await SecureStore.getItemAsync(key);
+    return await SecureStore.getItemAsync(key);
   } catch (error) {
     console.log("Error getting the auth token", error);
   }
@@ -29,8 +28,7 @@ const getUser = async (): Promise<User | null> => {
 
 const removeToken = async () => {
   try {
-    // await SecureStore.deleteItemAsync(key);
-    localStorage.removeItem(key);
+    await SecureStore.deleteItemAsync(key);
   } catch (error) {
     console.log("Error removing the auth token", error);
   }
