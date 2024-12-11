d2979025baea9e394e1794ae4d898a6cd16890ad
augustine-awuori <codewithaugustine@gmail.com>
2024-12-11 12:53:39 +0300

    Control app's status bar

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,7 @@
 // make sure gesture-handler import is at the top and there's nothing else before it
 import "./gesture-handler";
 import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { StreamApp } from "expo-activity-feed";
 import { STREAM_API_KEY, STREAM_APP_ID } from "@env";
@@ -99,6 +100,7 @@ export default function App() {
                   value={{ profileUser, setProfileUser }}
                 >
                   <AppNavigator />
+                  <StatusBar style="auto" />
                 </ProfileUserContext.Provider>
               </SparklesContext.Provider>
             </UserContext.Provider>
