d9cdf26f0476228a37d44b9477e77eca2c32a411
augustine-awuori <codewithaugustine@gmail.com>
2024-12-18 11:38:16 +0300

    Feature: show sparkles for the mentions notifications

--- app/components/notification/MentionNotification.tsx
+++ app/components/notification/MentionNotification.tsx
@@ -1,54 +1,23 @@
-import { useState } from "react";
-import Icon from "@expo/vector-icons/Feather";
+import { View } from 'react-native';
+import { NotificationActivity } from 'getstream';
 
-import { NotificationActivity } from "getstream";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { Sparkle } from "../sparkle";
-import colors from "../../config/colors";
-import Notification from "./Notification";
-import Text from "../Text";
+import { Sparkle } from '../sparkle';
 
 interface Props {
   activityGroup: NotificationActivity;
 }
 
 export default ({ activityGroup }: Props) => {
-  const [showSparkles, setShowSparkles] = useState(false);
-
   const { activities, verb } = activityGroup;
 
-  const MentionSparkles = (
-    <View>
-      <TouchableOpacity onPress={() => setShowSparkles(!showSparkles)}>
-        <Text style={styles.text}>
-          {showSparkles ? "Hide" : "Show"} sparkles
-        </Text>
-      </TouchableOpacity>
-
-      {showSparkles &&
-        activities.map((sparkle) => (
+  if (verb === 'tweet' || verb === 'sparkle')
+    return (
+      <View>
+        {activities.map((sparkle) => (
           <Sparkle key={sparkle.id} activity={sparkle} />
         ))}
-    </View>
-  );
-
-  if (verb === "tweet" || verb === "sparkle")
-    return (
-      <Notification
-        action="mentioned you in their sparkle"
-        Icon={<Icon name="at-sign" size={24} color="black" />}
-        activityGroup={activityGroup}
-        onPress={() => setShowSparkles(true)}
-        Other={MentionSparkles}
-      />
+      </View>
     );
 
   return null;
 };
-
-const styles = StyleSheet.create({
-  text: {
-    color: colors.blue,
-    marginBottom: 5,
-  },
-});
