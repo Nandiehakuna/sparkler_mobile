fb7665b1c6f38209492a0ad54bec80646c002d46
augustine-awuori <codewithaugustine@gmail.com>
2024-12-21 10:44:11 +0300

    Improve the look and feel: have a better home icon

--- app/components/icons/SearchIcon.tsx
+++ app/components/icons/SearchIcon.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { OpaqueColorValue } from "react-native";
-import Icon from "@expo/vector-icons/Ionicons";
+import { OpaqueColorValue } from 'react-native';
+import Icon from '@expo/vector-icons/FontAwesome';
 
-import colors from "../../config/colors";
+import colors from '../../config/colors';
 
 interface Props {
   color?: string | OpaqueColorValue | undefined;
@@ -10,12 +9,6 @@ interface Props {
   focused?: boolean;
 }
 
-export default ({ color = colors.medium, focused, size = 18 }: Props) => {
-  return (
-    <Icon
-      name={focused ? "search" : "search-outline"}
-      size={size}
-      color={color}
-    />
-  );
-};
+export default ({ color = colors.medium, size = 18 }: Props) => (
+  <Icon name="search" size={size} color={color} />
+);
