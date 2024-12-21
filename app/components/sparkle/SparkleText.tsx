0abfb88dac9a3908528eae86d7d33f2d43df85f1
augustine-awuori <codewithaugustine@gmail.com>
2024-12-21 12:11:23 +0300

    Improve the 'Read more' text for the sparkle

--- app/components/sparkle/SparkleText.tsx
+++ app/components/sparkle/SparkleText.tsx
@@ -1,29 +1,54 @@
-import { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, TextProps } from 'react-native';
+import React from 'react';
+import { View, Pressable, StyleSheet } from 'react-native';
 
-import { routes } from '../../navigation';
 import { getActorFromUser } from '../../utils/funcs';
+import { routes } from '../../navigation';
 import { useNavigation, useUsers } from '../../hooks';
 import colors from '../../config/colors';
 import Text from '../Text';
 
-interface Props extends TextProps {
-  children: string;
-  numberOfLines: number;
-  onReadMore: (value: boolean) => void;
+interface Part {
+  text: string;
+  isMention?: boolean;
+  isHashtag?: boolean;
 }
 
-const TruncatedText = (props: Props) => {
-  const [isTruncated, setIsTruncated] = useState(false);
+interface Props {
+  onReadMore: () => void;
+  text: string;
+}
+
+const CustomTextComponent: React.FC<Props> = ({ onReadMore, text }) => {
   const { usernameIdMap, idUserMap } = useUsers();
   const navigation = useNavigation();
 
-  const { children, numberOfLines, onReadMore } = props;
+  const parseText = (str: string): Part[] => {
+    const parts: Part[] = [];
+    let match;
+    let lastIndex = 0;
 
-  const handleToggleText = () => {
-    setIsTruncated(!isTruncated);
-    onReadMore?.(!isTruncated);
+    const regex = /(^|\s)(#[a-z\d-]+|[@][a-z\d-]+)/gi; // Regex for hashtags and mentions
+
+    while ((match = regex.exec(str)) !== null) {
+      if (match.index > lastIndex) {
+        parts.push({ text: str.slice(lastIndex, match.index) });
+      }
+
+      parts.push({
+        text: match[2],
+        isMention: match[2].startsWith('@'),
+        isHashtag: match[2].startsWith('#'),
+      });
+
+      lastIndex = regex.lastIndex;
+    }
+
+    if (lastIndex < str.length) parts.push({ text: str.slice(lastIndex) });
+
+    return parts;
   };
+
+  const parsedText = parseText(text);
 
   const handleHashtagPress = (hashtag: string) =>
     navigation.navigate(routes.HASHTAG, { hashtag });
@@ -36,70 +61,48 @@ const TruncatedText = (props: Props) => {
     navigation.navigate(routes.PROFILE, getActorFromUser(user));
   };
 
-  const renderText = (text: string) => {
-    const parts = text.split(/(\s+)/).map((part, index) => {
-      if (part.startsWith('#'))
-        return (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handleHashtagPress(part)}
-          >
-            <Text style={styles.link}>{part}</Text>
-          </TouchableOpacity>
-        );
-      else if (part.startsWith('@'))
-        return (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handleMentionPress(part)}
-          >
-            <Text style={styles.link}>{part}</Text>
-          </TouchableOpacity>
-        );
-
-      return part;
-    });
-
-    return parts;
+  const handlePress = (item: Part) => {
+    if (item.isMention) {
+      const mentionWithoutSign = item.text.slice(1);
+      handleMentionPress(mentionWithoutSign);
+    } else if (item.isHashtag) {
+      const hashtagWithoutSign = item.text.slice(1);
+      handleHashtagPress(hashtagWithoutSign);
+    }
   };
 
   return (
-    <View>
-      <Text
-        numberOfLines={isTruncated ? undefined : numberOfLines}
-        style={styles.text}
-      >
-        {renderText(children)}
-        {!isTruncated && (
-          <TouchableOpacity onPress={handleToggleText}>
-            <Text style={styles.readMore}> Read more</Text>
-          </TouchableOpacity>
-        )}
-      </Text>
+    <View style={styles.container}>
+      {parsedText.map((part, index) =>
+        part.isMention || part.isHashtag ? (
+          <Pressable key={index} onPress={() => handlePress(part)}>
+            <Text style={styles.highlightedText}>{part.text}</Text>
+          </Pressable>
+        ) : (
+          <Text key={index}>{part.text}</Text>
+        ),
+      )}
+      {text.length > 280 && (
+        <Pressable onPress={onReadMore}>
+          <Text style={styles.readMore}>Read more</Text>
+        </Pressable>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 15,
-    color: colors.medium,
-    lineHeight: 20,
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  link: {
-    fontSize: 15,
+  highlightedText: {
     color: colors.blue,
   },
   readMore: {
-    fontSize: 14,
     color: colors.blue,
-    fontWeight: '500',
-  },
-  showLess: {
-    fontSize: 14,
-    color: 'blue',
-    marginTop: 4,
+    marginLeft: 5,
   },
 });
 
-export default TruncatedText;
+export default CustomTextComponent;
