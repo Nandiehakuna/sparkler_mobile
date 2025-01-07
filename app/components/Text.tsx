0cf9ee87121c5be1e2004dc222be6f37eeb507c4
augustine-awuori <codewithaugustine@gmail.com>
2025-01-07 13:28:53 +0300

    Improve the look and feel: use the right font family for the bold text

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -1,27 +1,24 @@
-import { StyleProp, Text, TextProps, TextStyle } from 'react-native';
+import { StyleProp, Text as RNText, TextProps, TextStyle } from 'react-native';
 
 interface Props extends TextProps {
   isBold?: boolean;
 }
 
 export default (props: Props) => {
-  const { children, isBold: useBoldFontFamily, style = {}, ...rest } = props;
+  const { children, isBold, style = {}, ...rest } = props;
 
   const getStyle = (): StyleProp<TextStyle> => {
     return [
       style,
       {
-        fontFamily: useBoldFontFamily
-          ? 'Quicksand_700Bold'
-          : 'Quicksand_400Regular',
-        fontWeight: useBoldFontFamily ? '700' : '400',
+        fontFamily: isBold ? 'Quicksand_700Bold' : 'Quicksand_400Regular',
       },
     ];
   };
 
   return (
-    <Text style={getStyle()} {...rest}>
+    <RNText style={getStyle()} {...rest}>
       {children}
-    </Text>
+    </RNText>
   );
 };
