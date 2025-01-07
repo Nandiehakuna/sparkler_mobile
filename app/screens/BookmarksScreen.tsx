48cad04345fe89e86b7d5947cc0d50d3ef245170
augustine-awuori <codewithaugustine@gmail.com>
2025-01-07 19:04:04 +0300

    Give feedback on bookmarks retrieval

--- app/screens/BookmarksScreen.tsx
+++ app/screens/BookmarksScreen.tsx
@@ -2,34 +2,43 @@ import { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { Activity } from 'getstream';
 
-import { Sparkle, Text } from '../components';
+import { ActivityIndicator, Sparkle, Text } from '../components';
 import { SparkleActivity } from '../utils/types';
 import { useBookmark } from '../hooks';
 import colors from '../config/colors';
 
 export default () => {
+  const [loading, setIsLoading] = useState(false);
   const [bookmarks, setBookmarks] = useState<SparkleActivity[]>([]);
   const { getBookmarkedSparkles } = useBookmark();
 
   useEffect(() => {
     async function initBookmarks() {
+      setIsLoading(true);
       setBookmarks(await getBookmarkedSparkles());
+      setIsLoading(false);
     }
 
     initBookmarks();
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text isBold style={styles.title}>
-        Bookmarks
-      </Text>
-      <FlatList
-        data={bookmarks || []}
-        keyExtractor={(bookmark) => bookmark.id}
-        renderItem={({ item }) => <Sparkle activity={item as unknown as Activity} />}
-      />
-    </View>
+    <>
+      <ActivityIndicator visible={loading} />
+      <View style={styles.container}>
+        <Text isBold style={styles.title}>
+          Bookmarks
+        </Text>
+        {!bookmarks.length && !loading && (
+          <Text style={styles.text}>You don't have any bookmarks yet</Text>
+        )}
+        <FlatList
+          data={bookmarks || []}
+          keyExtractor={(bookmark) => bookmark.id}
+          renderItem={({ item }) => <Sparkle activity={item as unknown as Activity} />}
+        />
+      </View>
+    </>
   );
 };
 
@@ -38,9 +47,14 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     flex: 1,
   },
+  text: {
+    marginTop: 30,
+    textAlign: 'center',
+  },
   title: {
     textAlign: 'center',
     fontSize: 24,
     marginBottom: 15,
+    marginTop: 7,
   },
 });
