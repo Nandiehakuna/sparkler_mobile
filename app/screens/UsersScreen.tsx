e26a270b7d48b0aa1a7e01f9b171ef63f4bd306b
augustine-awuori <codewithaugustine@gmail.com>
2025-01-23 14:14:33 +0300

    Revert: code refactor

--- app/screens/UsersScreen.tsx
+++ app/screens/UsersScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import {
@@ -8,36 +8,16 @@ import {
   UserCard,
   UserCardSeparator,
 } from '../components';
-import { getValidUsers, getVerifiedFirst } from '../hooks/useUsers';
 import { routes } from '../navigation';
 import { ScreenProps } from '../utils/types';
-import { useTheme, useToast } from '../hooks';
-import { User } from '../contexts/UsersContext';
-import usersApi from '../api/users';
+import { useTheme, useUsers } from '../hooks';
 
 export default ({ navigation }: ScreenProps) => {
   const [searchQuery, setSearchQuery] = useState('');
   const { theme } = useTheme();
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState<User[]>([]);
-  const toast = useToast();
+  const { users: allUsers, isLoading } = useUsers();
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setLoading(true);
-        const res = await usersApi.getAllUsers();
-        if (res.ok) setUsers(getVerifiedFirst(getValidUsers(res.data as User[])));
-        setLoading(false);
-      } catch (error) {
-        toast.show('Error fetching users', 'error');
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  const filteredUsers = users.filter(
+  const filteredUsers = allUsers.filter(
     ({ name, username }) =>
       name.toLowerCase().includes(searchQuery.toLowerCase()) ||
       username.toLowerCase().includes(searchQuery.toLowerCase())
@@ -45,7 +25,7 @@ export default ({ navigation }: ScreenProps) => {
   // TODO: add a retry button
   return (
     <>
-      <ActivityIndicator visible={loading} />
+      <ActivityIndicator visible={isLoading} />
       <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
         <SearchInput
           onSearchQueryChange={setSearchQuery}
