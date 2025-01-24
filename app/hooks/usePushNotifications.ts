8797573ab61fcde2a9fb55350921995b331a8ce1
augustine-awuori <codewithaugustine@gmail.com>
2025-01-24 09:49:12 +0300

    Bug fix: use the right credentials for push notifications

--- app/hooks/usePushNotifications.ts
+++ app/hooks/usePushNotifications.ts
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Device from 'expo-device';
+import Constants from 'expo-constants';
 
 import expoPushTokensApi from '../api/expoPushTokens';
 import useUser from './useUser';
@@ -17,12 +19,23 @@ export default (notificationListener?: (event: Notifications.NotificationRespons
 
   const registerForPushNotifications = async () => {
     try {
+      if (Platform.OS === 'android')
+        await Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: '#ff231f7c',
+        });
+
       if (!Device.isDevice) return;
 
       const { granted } = await Notifications.requestPermissionsAsync();
       if (!granted) return;
 
-      const token = await Notifications.getExpoPushTokenAsync();
+      const projectId: string | undefined = Constants?.expoConfig?.extra?.eas?.projectId;
+      const token = await Notifications.getExpoPushTokenAsync({
+        projectId,
+      });
 
       if (!user?.expoPushToken || user?.expoPushToken?.data !== token.data)
         expoPushTokensApi.register(token);
