8ae6ea21383cdbc80e340b41f78c29eff4567fb1
augustine-awuori <codewithaugustine@gmail.com>
2025-01-24 10:23:09 +0300

    Bug fix: load the bookmarks screen

--- app/screens/BookmarksScreen.tsx
+++ app/screens/BookmarksScreen.tsx
@@ -2,38 +2,30 @@ import { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { Activity } from 'getstream';
 
-import { ActivityIndicator, AppRefreshControl, RetryButton, Sparkle, Text } from '../components';
+import { ActivityIndicator, Sparkle, Text } from '../components';
 import { SparkleActivity } from '../utils/types';
 import { useBookmark, useTheme, useUser } from '../hooks';
 
 export default () => {
   const [loading, setIsLoading] = useState(false);
-  const [errorOccurred, setErrorOccurred] = useState(false);
   const [bookmarks, setBookmarks] = useState<SparkleActivity[]>([]);
   const { getBookmarkedSparkles } = useBookmark();
   const { theme } = useTheme();
   const { user } = useUser();
 
   useEffect(() => {
-    if (user) initBookmarks();
-  }, [user]);
-
-  useEffect(() => {
-    if (errorOccurred && user) initBookmarks();
-  }, [errorOccurred, user]);
-
-  async function initBookmarks() {
-    try {
-      if (!user) return;
-      setErrorOccurred(false);
-
-      setIsLoading(true);
-      setBookmarks(await getBookmarkedSparkles());
-      setIsLoading(false);
-    } catch (error) {
-      setErrorOccurred(true);
+    async function initBookmarks() {
+      try {
+        if (user) {
+          setIsLoading(true);
+          setBookmarks(await getBookmarkedSparkles());
+          setIsLoading(false);
+        }
+      } catch (error) {}
     }
-  }
+
+    initBookmarks();
+  }, []);
 
   return (
     <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
@@ -42,20 +34,13 @@ export default () => {
         <Text isBold style={styles.title}>
           Bookmarks
         </Text>
-
         {!bookmarks.length && !loading && (
           <Text style={styles.text}>You don't have any bookmarks yet</Text>
         )}
-
         <FlatList
           data={bookmarks || []}
           keyExtractor={(bookmark) => bookmark.id}
           renderItem={({ item }) => <Sparkle activity={item as unknown as Activity} />}
-          refreshControl={<AppRefreshControl onRefresh={initBookmarks} />}
-          ListHeaderComponent={
-            <RetryButton onPress={initBookmarks} visible={errorOccurred || !bookmarks.length} />
-          }
-          style={styles.container}
         />
       </View>
     </View>
