35adbe054169b7087437a75c41003cd94814cdde
augustine-awuori <codewithaugustine@gmail.com>
2025-02-03 08:57:28 +0300

    Bug fix: use appropriate icon

--- app/components/icons/LinkedInIcon.tsx
+++ app/components/icons/LinkedInIcon.tsx
@@ -1,5 +1,5 @@
 import { OpaqueColorValue } from 'react-native';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 import { useTheme } from '../../hooks';
 
@@ -12,5 +12,5 @@ interface Props {
 export default ({ color, size = 18 }: Props) => {
   const { theme } = useTheme();
 
-  return <FontAwesome5 name="linkedIn" size={size} color={color || theme.colors.text} />;
+  return <AntDesign name="linkedin-square" size={size} color={color || theme.colors.text} />;
 };
