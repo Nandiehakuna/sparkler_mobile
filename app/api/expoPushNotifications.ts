786cbf0a1f4663ffe4e94d01ac2392628c226685
augustine-awuori <codewithaugustine@gmail.com>
2025-02-03 14:22:43 +0300

    Add title to push notifications

--- app/api/expoPushNotifications.ts
+++ app/api/expoPushNotifications.ts
@@ -2,7 +2,7 @@ import client, { getFailedResponse, processResponse } from './client';
 
 const endpoint = '/expoPushNotifications';
 
-const send = async (data: { targetUsersId: string[]; message: string }) => {
+const send = async (data: { targetUsersId: string[]; message: string; title?: string }) => {
   try {
     return processResponse(await client.post(endpoint, data));
   } catch (error) {
